e4088a3f794e5e45c755d6ff6ce6f98202779fec
RiccardoVoltolina <riky.volto@live.com>
2024-01-12 10:19:21 +0100

    start react introduction game

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import { useState } from 'react';
+
+import axios from 'axios'
+
 
 function App() {
-  
+  function Square() {
+    return <button className="square">X</button>;
+  }
   return (
     <>
-     
+      <div><Square /></div>
     </>
   )
 }
